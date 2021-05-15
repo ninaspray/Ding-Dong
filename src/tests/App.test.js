edcbf3f76bfa9576a6122860fd290e46c9b30967
ninaspray <ninaspray@gmail.com>
2021-05-15 16:34:43 +0100

    Add: Ding Dong Repo Set Up

---
+++ src/tests/App.test.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import App from '../components/App';
+
+describe("App", () => {
+  it("renders correctly", () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});;
