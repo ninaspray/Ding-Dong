a7172e2ca1280a0ce94ef3d9b51b8d6bb65e35f9
ninaspray <ninaspray@gmail.com>
2021-05-22 14:08:34 +0100

    Form working

--- src/components/CreateTennant.js
+++ src/components/CreateTennant.js
@@ -1,20 +1,27 @@
 import React from "react";
 
+
 //custom Hooks
 import { useForm } from '../hooks/useForm';
 
 
 const CreateTennant = () => {
 
-  const [{fullname, email, telephone }, setInput] = useForm({
+  const [{fullname, email, telephone, flat_number }, setInput] = useForm({
     fullname: '',
     email: '',
     telephone: '',
+    flat_number: '',
   });
 
+  const submitForm = (e) => {
+    e.preventDefault()
+    console.log("calling",fullname, email, telephone, flat_number)
+  }
+ 
   return ( 
 
-    <form>
+    <form onSubmit={submitForm}>
     <input
     type="text"
     name="fullname"
@@ -34,6 +41,13 @@ const CreateTennant = () => {
     name="telephone"
     placeholder="Telephone"
     value={telephone}
+    onChange={setInput}
+    />
+    <input
+    type="text"
+    name="flat_number"
+    placeholder="Flat Number"
+    value={flat_number}
     onChange={setInput}
     />
     <button type="submit">Submit</button>
