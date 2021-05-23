5b87fd6710ddda5363f54d551727388f67211533
ninaspray <ninaspray@gmail.com>
2021-05-23 11:57:06 +0100

    Add: Ding Dong Button Added

--- src/components/DingDong.js
+++ src/components/DingDong.js
@@ -1,17 +1,49 @@
 import React from 'react';
+import newParcel from '../services/newParcel';
 
+//custom Hooks
+import { useForm } from '../hooks/useForm';
 
-const sendDingDong = () => {
+    
 
-return (
+const SendDingDong = () => {
+
+    const [{size}, setInput] = useForm({
+    size: '',
+  });
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    newParcel({size});
+    console.log(newParcel)
+  }
+ 
+  return (  
         <>
-            <form>
-                <button type="submit">
-                    Log In
-                </button>
-            </form>
-        </>
-    );
-}
+        <h1>   
+        Find Tennant Function
+        </h1>
+      
+    <form onSubmit={submitForm}>
+    <label htmlFor="Type">
+            Parcel Size
+    <select
+              placeholder="Parcel Size"
+              name="size"
+              value={size}
+              onChange={setInput}
+            >
+             <option value="Small">Small</option>
+             <option value="Medium">Medium</option>
+             <option value="Large">Large</option>
+            </select>
+            </label>        
 
-export default sendDingDong;
+    <button type="submit">Send Ding Dong</button>
+    </form>
+    </>
+  );
+};
+
+
+export default SendDingDong
