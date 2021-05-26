bc5485de9fe1f2459d04059d12a80e1fbdbbd019
Joanna Hughes <joannahughes@hotmail.co.uk>
2021-05-26 20:31:55 +0100

    layout changes

--- src/components/App.js
+++ src/components/App.js
@@ -16,33 +16,36 @@ import { AuthProvider } from '../contexts/AuthContext';
 //<Route exact path="/tennant" component={Tennants}/>
 import DingDongLogo from '../Images/DingDongLogo.png';
 import '../styles/App.css';
+import '../styles/Footer.css';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <header className="App-header">
-          <NavBar />
-          <h2>Ding Dong</h2>
-          <div>
-            <img src={DingDongLogo} width='300px' height='350px' alt='DingDongLogo' />
-          </div>
-        </header>
-        <main className="App-content">
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            {/* <Route exact path="/home" component={accountDetails}/> */}
-            {/* <Route exact path="/Contact" component={contactConcierge} /> */}
-            <Route exact path="/create" component={CreateTennant} />
-            {/* <Route exact path="/SendDingDong" component={sendDingDong} />
-            <Route exact path="/ParcelCollected" component={collectedParcel} /> */}
-            <Route exact path="/sign-up" component={Signup} />
-          </Switch>
-        </main>
-      </Router>
-      <footer className="App-footer">
-      </footer>
-    </AuthProvider>
+    <div className="App">
+      <AuthProvider>
+        <Router>
+          <header className="App-header">
+            <NavBar />
+            <h2>Ding Dong</h2>
+            <div>
+              <img src={DingDongLogo} width='300px' height='350px' alt='DingDongLogo' />
+            </div>
+          </header>
+          <main className="App-content">
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              {/* <Route exact path="/home" component={accountDetails}/> */}
+              {/* <Route exact path="/Contact" component={contactConcierge} /> */}
+              <Route exact path="/create" component={CreateTennant} />
+              {/* <Route exact path="/SendDingDong" component={sendDingDong} />
+              <Route exact path="/ParcelCollected" component={collectedParcel} /> */}
+              <Route exact path="/sign-up" component={Signup} />
+            </Switch>
+          </main>
+        </Router>
+        <footer className="App-footer">
+        </footer>
+      </AuthProvider>
+    </div>
     );
   }
 
