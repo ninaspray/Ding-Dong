df7d46c167fd70dde2aff174e48a23b2676fc32d
ninaspray <ninaspray@gmail.com>
2021-06-03 20:42:50 +0100

    Fix: Tennant Row

--- src/components/TenantList.js
+++ src/components/TenantList.js
@@ -1,32 +1,7 @@
 import React, { useState, useEffect } from "react";
 import getTennant from "../requests/getTennant";
 import TennantRow from "./TennantRow";
-import SendDingDong from "./SendDingDong";
-//import sendDingDong from './DingDong';
-import Collected from "./Collection";
-// DingDongCTA props => tenant id
 import "../styles/Table.css";
-
-const TennantRow = ({ tennant, addParcel }) => {
-  const { firstname, lastname, flat_number, id, packages } = tennant;
-  const filtered = packages.filter((pack) => pack?.collected === false);
-  return (
-    <tr>
-      <td>{firstname}</td>
-      <td>{lastname}</td>
-      <td>{flat_number}</td>
-      <td className="table__send-dingdong">
-        {filtered.length === 0 ? (
-          <SendDingDong tennantId={id} addParcel={addParcel} />
-        ) : (
-          <Collected
-            packages={filtered} /*updateCollection={updateCollection}*/
-          ></Collected>
-        )}
-      </td>
-    </tr>
-  );
-};
 
 const TenantList = () => {
   const [list, setList] = useState([]);
